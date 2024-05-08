f8a1c6e5ec87d53aed249b3a96e770c4daf96df0
Muhammad Tameem Gazi <116261279+Tameem2004@users.noreply.github.com>
2024-05-09 01:51:06 +0530

    Update polynomialEvaluation.cpp

--- polynomialEvaluation.cpp
+++ polynomialEvaluation.cpp
@@ -1,8 +1,9 @@
-#include <bits/stdc++.h>
+#include <iostream>
 
 using namespace std;
 
-int polynomialEvaluation(int x, int n,int A[]){
+int polynomialEvaluation(int x, int n,int A[])
+{
     int i=n;
     int P=A[i];
     while (i>0)
@@ -14,17 +15,27 @@ int polynomialEvaluation(int x, int n,int A[]){
 
 int main()
 {
-    int x, n;
+    int x, n, i;
     cout << "\nEnter The Value Of x of The Polynomial P(x) : ";
     cin >> x;
     cout << "\nEnter The Highest Power Of The Polynomial : ";
     cin >> n;
     int A[n+1];
     cout<<"Enter The Coefficient Matrix : "<<endl;
-    for (int i = 0; i <= n; i++)
+    for (i = 0; i <= n; i++)
     {
         cout<<"A("<<i<<") : ";
         cin>>A[i];
+    }
+    cout<<"\nThe Equation: ";
+    for(i=n; i>=0; i--)
+    {
+        if(A[i]<0)
+        cout<<" "<<A[i]<<"x^"<<i;
+        else if(i==n)
+        cout<<A[i]<<"x^"<<i;
+        else
+        cout<<" + "<<A[i]<<"x^"<<i;
     }
     int result = polynomialEvaluation(x, n,A);
     cout<<"\nThe Result of P("<<x<<") With Highest Power "<<n<<" is : ";
