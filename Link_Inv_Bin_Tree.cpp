ce2faecea432e7cec7342140b899ab23f4f37a64
Muhammad Tameem Gazi <116261279+Tameem2004@users.noreply.github.com>
2024-05-08 22:44:50 +0530

    Update Link_Inv_Bin_Tree.cpp

--- Link_Inv_Bin_Tree.cpp
+++ Link_Inv_Bin_Tree.cpp
@@ -1,243 +1,245 @@
-#include <bits/stdc++.h>
+#include<iostream>
 using namespace std;
 
-struct Node
-{
-    int data, tag;
-    struct Node *left;
-    struct Node *right;
-    Node(int data)
-    {
-        this->data = data;
-        this->tag = 0;
-        left = NULL;
-        right = NULL;
-    }
-};
+typedef struct Node{
+    int data;
+    int tag;
+    struct Node*left;
+    struct Node*right;
+}Node;
 
-class Bin_S_Tree
+Node* creatnewNode(int data)
 {
-private:
-    Node *pres = NULL;
-    Node *createNode(int data)
-    {
-        Node *temp = new Node(data);
-        temp->right = NULL;
-        temp->left = NULL;
-        return temp;
-    }
-    Node *insert(Node *pres, int key)
-    {
-        if (pres == NULL)
-        {
-            pres = createNode(key);
-            return pres;
-        }
-        if (key > (pres)->data)
-            (pres)->right = insert((pres)->right, key);
-        else if (key <= (pres)->data)
-            (pres)->left = insert((pres)->left, key);
-        return pres;
-    }
-    Node *descend_left(Node *prev)
-    {
-        Node *temp = pres->left;
-        pres->left = prev;
-        pres->tag = 0;
-        prev = pres;
-        pres = temp;
-        return prev;
-    }
-    Node *descend_right(Node *prev)
-    {
-        Node *temp = pres->right;
-        pres->right = prev;
-        pres->tag = 1;
-        prev = pres;
-        pres = temp;
-        return prev;
-    }
-    Node *ascend_left(Node *prev)
-    {
-        Node *temp = prev->left;
-        prev->left = pres;
-        pres = prev;
-        prev = temp;
-        return prev;
-    }
-    Node *ascend_right(Node *prev)
-    {
-        Node *temp = prev->right;
-        prev->right = pres;
-        pres = prev;
-        prev = temp;
-        return prev;
-    }
+    Node *newNode=new Node;
+    newNode->data=data;
+    newNode->left=NULL;
+    newNode->right=NULL;
+    return newNode;
+}
 
-public:
-    void setTree(int data)
+void visit_pres(int data) {
+    cout << data << " ";
+}
+
+Node* link_inversion_inorder_binarytree(Node *pres)
+{
+    cout<<"Link Inversion Inorder:-"<<endl;
+    Node *prev,*tmp;
+    prev=NULL;
+    int flag=0;
+    if(pres==NULL)
     {
-        pres = insert(pres, data);
+        return 0;
     }
-    void getInOrderTree()
+    do
     {
-        Node *prev;
-        prev = NULL;
-        int flag = 0;
-        if (pres == NULL)
+        if(pres->left!=NULL&&flag==0)
         {
-            cout << "No Data In The Tree" << endl;
-            return;
+            tmp=pres->left;
+            pres->left=prev;
+            pres->tag=0;
+            prev=pres;
+            pres=tmp;
+            flag=0;
         }
-        do
-        {
-            if (pres->left != NULL && flag == 0)
+        else{
+            if(flag!=2)
             {
-                prev = descend_left(prev); // descend left child
-                flag = 0;
-            }
-            else
-            {
-                if (flag != 2)
+                visit_pres(pres->data);
+                if(prev==NULL&&pres->right==NULL)
                 {
-                    cout <<pres->data<< "\t"; // visit pres
-                    if (prev == NULL && pres->right == NULL)
-                    {
-                        flag = 2;
-                    }
-                }
-                if (pres->right != NULL && flag != 2)
-                {
-                    prev = descend_right(prev);
-                    flag = 0;
-                }
-                else if (prev != NULL && prev->tag == 0)
-                {
-                    prev = ascend_left(prev); // ascend from the left subtree
-                    flag = 1;
-                }
-                else if (prev != NULL && prev->tag == 1)
-                {
-                    prev = ascend_right(prev); // ascend from the right subtree
-                    flag = 2;
+                    flag=2;
                 }
             }
-        } while (prev != NULL || flag != 2);
-    }
-    void getPreOrderTree()
-    {
-        Node *prev = NULL;
-        int flag = 0;
-        if (pres == NULL)
-        {
-            cout << "No Data In The Tree";
-            return;
+            if(pres->right!=NULL&&flag!=2)
+            {
+                tmp=pres->right;
+                pres->right=prev;
+                pres->tag=1;
+                prev=pres;
+                pres=tmp;
+                flag=0;
+            }
+            else if(prev!=NULL&&prev->tag==0)
+            {
+                tmp=prev->left;
+                prev->left=pres;
+                pres=prev;
+                prev=tmp;
+                flag=1;
+            }
+            else if(prev!=NULL&&prev->tag==1)
+            {
+                tmp=prev->right;
+                prev->right=pres;
+                pres=prev;
+                prev=tmp;
+                flag=2;
+            }
         }
-        do
-        {
-            if (flag == 0)
-            {
-                cout << pres->data << "\t"; // visit pres
-                if (prev == NULL && pres->right == NULL && pres->left == NULL)
-                    flag = 2;
-            }
-            if (pres->left != NULL && flag == 0)
-            {
-                prev = descend_left(prev); // descend left child
-                flag = 0;
-            }
-            else
-            {
-                if (pres->right != NULL && flag != 2)
-                {
-                    prev = descend_right(prev); // descend right child
-                    flag = 0;
-                }
-                else if (prev != NULL && prev->tag == 0)
-                {
-                    prev = ascend_left(prev); // ascend from the left subtree
-                    if (prev == NULL && pres->right == NULL)
-                    {
-                        flag = 2;
-                    }
-                    else
-                        flag = 1;
-                }
-                else if (prev != NULL && prev->tag == 1)
-                {
-                    prev = ascend_right(prev); // ascend from the right subtree
-                    flag = 2;
-                }
-            }
-        } while (prev != NULL || flag != 2);
     }
-    void getPostOrderTree()
+    while(prev!=NULL||flag!=2);
+    cout<<endl;
+}
+
+Node* link_inversion_preorder_binarytree(Node *pres)
+{
+    cout<<"Link Inversion Preorder:-"<<endl;
+    Node *prev,*tmp;
+    prev=NULL;
+    int flag=0; 
+    if(pres==NULL)
     {
-        Node *prev = NULL;
-        int flag = 0;
-        if (pres == NULL)
-        {
-            cout << "No Data In The Tree";
-            return;
-        }
-        do
-        {
-            if (pres->left != NULL && flag == 0)
-            {
-                prev = descend_left(prev); // descend left child
-                flag = 0;
-            }
-            else
-            {
-                if (pres->right != NULL && flag != 2)
-                {
-                    prev = descend_right(prev); // descend right child
-                    flag = 0;
-                }
-                else if (prev != NULL && prev->tag == 0)
-                {
-                    prev = ascend_left(prev); // ascend from the left subtree
-                    if (prev == NULL && pres->right == NULL)
-                    {
-                        flag = 2;
-                    }
-                    else
-                        flag = 1;
-                }
-                else if (prev != NULL && prev->tag == 1)
-                {
-                    prev = ascend_right(prev); // ascend from the right subtree
-                    flag = 2;
-                }
-            }
-            if ((flag == 2) || (pres->right == NULL && flag == 1) || (pres->right == NULL && pres->left == NULL && flag == 0))
-            {
-                cout << pres->data <<"\t"; // visit pres
-                if (prev == NULL && pres->right == NULL &&  pres->left == NULL)
-                    flag = 2;
-            }
-        } while (prev != NULL || flag != 2);
+        return 0;
     }
-};
+    do
+    {
+        if(flag==0)
+        {
+            visit_pres(pres->data);
+        }
+        if(pres->left!=NULL&&flag==0)
+        {
+            tmp=pres->left;
+            pres->left=prev;
+            pres->tag=0;
+            prev=pres;
+            pres=tmp;
+            flag=0;
+        }
+        else
+        {
+            if(pres->right!=NULL&&flag!=2)
+            {
+                tmp=pres->right;
+                pres->right=prev;
+                pres->tag=1;
+                prev=pres;
+                pres=tmp;
+                flag=0;
+            }
+            else if(prev!=NULL&&prev->tag==0)
+            {
+                if(prev==NULL&&pres->left==NULL&&pres->right==NULL)
+                {
+                    flag=2;
+                }
+                else
+                {
+                    flag=1;
+                }
+                tmp=prev->left;
+                prev->left=pres;
+                pres=prev;
+                prev=tmp;
+            }
+            else if(prev!=NULL&&prev->tag==1)
+            {
+                tmp=prev->right;
+                prev->right=pres;
+                pres=prev;
+                prev=tmp;
+                flag=2;
+            }
+        }
+    }
+    while(prev!=NULL||flag!=2);
+    cout<<endl;
+}
+
+Node* link_inversion_postorder_binarytree(Node *pres)
+{
+    cout<<"Link Inversion Postorder:-"<<endl;
+    Node *prev,*tmp;
+    prev=NULL;
+    int flag=0;
+    if(pres==NULL)
+    {
+        return 0;
+    }
+    do
+    {
+        if(pres->left!=NULL&&flag==0)
+        {
+            tmp=pres->left;
+            pres->left=prev;
+            pres->tag=0;
+            prev=pres;
+            pres=tmp;
+            flag=0;
+        }
+        else
+        {
+            if(pres->right!=NULL&&flag!=2)
+            {
+                tmp=pres->right;
+                pres->right=prev;
+                pres->tag=1;
+                prev=pres;
+                pres=tmp;
+                flag=0;
+            }
+            else if(prev!=NULL&&prev->tag==0)
+            {
+                tmp=prev->left;
+                prev->left=pres;
+                pres=prev;
+                prev=tmp;
+                flag=1;
+            }
+            else if(prev!=NULL&&prev->tag==1)
+            {
+                tmp=prev->right;
+                prev->right=pres;
+                pres=prev;
+                prev=tmp;
+                flag=2;
+            }
+        }
+        if(flag==2||(pres->right==NULL&&flag==1)||(pres->left==NULL&&pres->right==NULL&&flag==0))
+        {
+            visit_pres(pres->data);
+            flag=2;
+        }
+    }
+    while(prev!=NULL||flag!=2);
+    cout<<endl;
+}
 
 int main()
 {
-    Bin_S_Tree Tree;
-    int n, data;
-    cout << "Enter number of elements you want to insert: ";
-    cin >> n;
-    cout << "\nEnter element to be inserted" << endl;
-    for (int i = 0; i < n; i++)
+    int ch;
+    Node *root=NULL;
+    root=creatnewNode(5);
+    root->left=creatnewNode(7);
+    root->right=creatnewNode(11);
+    root->left->left=creatnewNode(2);
+    root->left->right=creatnewNode(3);
+    root->left->right->left=creatnewNode(12);
+    root->right->right=creatnewNode(1);
+    while(1)
     {
-        cin >> data;
-        Tree.setTree(data);
+        cout<<"\nfor exit press: 0"<<"\nfor inorder: 1"<<"\nfor pre order: 2"<<"\nfor post order: 3"<<endl;
+        cout<<"Enter Your Choice"<<endl;
+        cin>>ch;
+        switch(ch)
+        {
+            case 0:
+                exit(1);
+                break;
+            case 1:
+                link_inversion_inorder_binarytree(root);
+                break;
+            case 2:
+                link_inversion_preorder_binarytree(root);
+                break;
+            case 3:
+                link_inversion_postorder_binarytree(root);
+                break;
+            default :
+                printf("you give a invalid choice");
+                break;
+        }
     }
-    cout << "Printing The Tree..." << endl;
-    cout << "Inorder ->" << endl;
-    Tree.getInOrderTree();
-    cout << "\nPreOrder ->" << endl;
-    Tree.getPreOrderTree();
-    cout << "\nPostorder ->" << endl;
-    Tree.getPostOrderTree();
     return 0;
 }
