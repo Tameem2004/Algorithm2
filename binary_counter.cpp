ccefac2cfd9bd3193340710e1d97922901878116
Muhammad Tameem Gazi <116261279+Tameem2004@users.noreply.github.com>
2024-05-08 21:12:34 +0530

    Update binary_counter.cpp

--- binary_counter.cpp
+++ binary_counter.cpp
@@ -1,30 +1,75 @@
-#include<iostream>
-#include <vector>
+#include <bits/stdc++.h>
+
 using namespace std;
+#define MAX 10
 
-void increment(vector<int>& A, int k) {
-    int i = 0;
-    for (int j =k-1; j >=0; j--) {
-        cout << A[j];
-    }
-    cout<<endl;
-    while (i < k && A[i] == 1)
+class binCounter
+{
+private:
+    int n,m;
+    int A[MAX];
+    void initA()
     {
-        A[i] = 0;
-        i += 1;
+        for (int i = 0; i < n; i++)
+        {
+            A[i] = 0;
+        }
+        return;
     }
-    if (i < k) {
-        A[i] = 1;
+    void getM(){
+        cout<<"Enter the Number To Count: "<<endl;
+        cin>>m;
     }
-}
+    void getN(){
+        n=0;
+        int temp=m;
+        while(temp>0){
+            temp/=2;
+            n++;
+        }
+    }
+public:
+    void count()
+    {
+        initA();
+            cout << 0 <<" = 0 ";
+            for (int j = n-2; j >= 0; j--)
+            {
+                cout << A[j] << " ";
+            }
+            cout<<endl;
+        for (int i = 0; i < m; i++)
+        {
+            int j = 0;
+            while (j < n)
+            {
+                if (A[j] == 0)
+                {
+                    A[j] = 1;
+                    j = n;
+                }
+                else
+                    A[j++] = 0;
+            }
+            cout << i+1 <<" = ";
+            for (int j = n-1; j >= 0; j--)
+            {
+                cout << A[j] << " ";
+            }
+            cout<<endl;
+        }
+    }
+    void countInput(){
+        getM();
+        getN();
+        count();
+        return;
+    }
+};
 
-int main() {
-    int k;
-
-    cout << "Enter Size Of Array" << endl;
-    cin >> k;
-    vector<int> A(k, 0);
-    for (int count = 0; count < (1 << k); count++) {
-        increment(A,k);
-    }
+int main()
+{
+    binCounter A;
+    A.countInput();
+    return 0;
 }
