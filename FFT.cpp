bc08f192bdef7658cd452a51524b0be38c307950
Muhammad Tameem Gazi <116261279+Tameem2004@users.noreply.github.com>
2024-05-09 13:20:42 +0530

    Update FFT.cpp

--- FFT.cpp
+++ FFT.cpp
@@ -1,92 +1,50 @@
 #include <iostream>
 #include <cmath>
 #include <complex>
-
 using namespace std;
-
-typedef complex<double> cd;
-
-const double pi = acos(-1);
-
-cd* FFT(const cd* S, int N, bool inverse) {
-    cd* A = new cd[N];
-    if (N == 1) {
-        A[0] = S[0];
-        return A;
-    }
-    else {
-        double power = -2.0 * pi / N * (inverse ? -1 : 1);
-        cd w(cos(power), sin(power));
-        cd p = 1.0;
-
-        cd* X = new cd[N/2];
-        cd* Y = new cd[N/2];
-
-        for (int i = 0; i < N / 2; i++) {
-            X[i] = S[2 * i];
-            Y[i] = S[2 * i + 1];
+#define PI 3.14159265358979323846
+void fft(complex<double> x[], int n, complex<double> A[]) {
+    if (n == 1) {
+        A[0] = x[0];
+    } else {
+        complex<double> even[n / 2];
+        complex<double> odd[n / 2];
+        complex<double> even_fft[n / 2];
+        complex<double> odd_fft[n / 2];
+        complex<double> Wn = exp(-2.0 * PI * complex<double>(0, 1) / static_cast<double>(n));
+        complex<double> w = 1;
+        int k;
+        for (k = 0; k < n / 2; k++) {
+            even[k] = x[2 * k];
+            odd[k] = x[2 * k + 1];
         }
 
-        cd* B = FFT(X, N / 2, inverse);
-        cd* C = FFT(Y, N / 2, inverse);
-
-        for (int k = 0; k < N / 2; k++) {
-            cd term = p * C[k];
-            A[k] = B[k] + term;
-            A[k + N / 2] = B[k] - term;
-            if (inverse) {
-                A[k] /= 2;
-                A[k + N / 2] /= 2;
-            }
-            p *= w;
+        fft(even, n / 2, even_fft);
+        fft(odd, n / 2, odd_fft);
+        for (k = 0; k < n / 2; k++) {
+            A[k] = even_fft[k] + w * odd_fft[k];
+            A[k + n / 2] = even_fft[k] - w * odd_fft[k];
+            w *= Wn;
         }
-        delete[] X;
-        delete[] Y;
-        delete[] B;
-        delete[] C;
-        return A;
     }
 }
-int main()
-{
-    int N;
-    cout << "Enter number of points: ";
-    cin >> N;
-    if ((log(N) / log(2) - (int)(log(N) / log(2))) > 0.0)
-    {
-        int temp = log(N) / log(2);
-        N = 2;
-        N = N << temp;
-    }
-    cd* S = new cd[N];
-    for (int i = 0; i < N; i++)
-    {
-        double var;
-        cout << "Enter " << i + 1 << " point: " << endl;
-        cout << "Real part: ";
-        cin >> var;
-        double imag;
-        cout << "Imaginary part: ";
-        cin >> imag;
-        S[i] = {var, imag};
-        cout << "Entered Number: " <<  S[i].real() << " + " <<  S[i].imag() << endl;
-    }
-    //FFT
-    cd* A = new cd[N];
-    A = FFT(S, N, false);
-    cout << "FFT:" << endl;
+int main() {
+    int n = 4; // Change n to your desired sequence length
+    complex<double> x[n]; // Input sequence
+    complex<double> A[n]; // Fourier coefficients
+    int i;
 
-    for (int i = 0; i < N; i++)
-    {
-        cout << i + 1 << "th result: " <<  A[i].real() << " + " <<  A[i].imag() << "i" << endl;
+    // Populate input sequence (can be changed according to requirements)
+    for (i = 0; i < n; i++) {
+        cout << "Enter f" << i << ": ";
+        cin >> x[i]; // Just a simple example, you can replace with your input sequence
     }
-    //Inverse FFT
-    cd* iA = new cd[N];
-    iA = FFT(A, N, true);
-    cout << "Inverse of the FFT:" << endl;
-    for (int i = 0; i < N; i++)
-    {
-        cout << i + 1 << "th result: " <<  iA[i].real() << " + " <<  iA[i].imag() << "i" << endl;
+    // Perform FFT
+    fft(x, n, A);
+    // Display the Fourier coefficients
+    cout << "Fourier coefficients:\n";
+    for (i = 0; i < n; i++) {
+        cout << "A" << i << " = " << A[i].real() << " + " << A[i].imag() << "i\n";
     }
     return 0;
 }
